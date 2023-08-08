108ce20b6e5369b896ec88240ede6529715d3f5a
kmkuiken <kmkuiken@gmail.com>
2023-08-08 15:11:27 -0400

    Created hover drawing effect
    Accessed each individual div using getElementsByClassName and added
    mouseover event check to change background color of touched div to black

--- draw.js
+++ draw.js
@@ -2,10 +2,18 @@ const board = document.querySelector(".container");
 
 let size = 16;
 
-for (i = 0; i < (size**2); i++) {
+for (let i = 0; i < (size**2); i++) {
   const box = document.createElement("div");
   box.style.cssText = `width: calc(50vh/${size}); height: calc(50vh/${size}); 
                        border-color: black; border-style: solid; border-width: 1px;`;
+  box.setAttribute("class", "box");
   board.appendChild(box);
 }
 
+let boxes = document.getElementsByClassName("box");
+
+for (let i = 0; i < boxes.length; i++) {
+  boxes[i].addEventListener("mouseover",  () => {
+    boxes[i].style.backgroundColor = "black";
+  });
+}
