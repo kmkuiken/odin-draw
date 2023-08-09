27c8c06188ebaedcad0980c4cde597eaf4b70a6b
kmkuiken <kmkuiken@gmail.com>
2023-08-09 14:48:45 -0400

    Added slider for artboard resolution
    Checking if the user has changed the slider and clicked the
    cooresponding button, the artboard is then removed of all previous
    divs and new ones are created based on the size chosen.

--- draw.js
+++ draw.js
@@ -1,16 +1,17 @@
 const board = document.querySelector(".container");
+const sizeConfirm = document.querySelector("#board-size-confirm");
+const sizeInput = document.querySelector("#board-size");
+let size = sizeInput.value;
+let defaultSize = 16;
+let boxes = document.getElementsByClassName("box");
 
-let size = 16;
-
-for (let i = 0; i < (size**2); i++) {
+for (let i = 0; i < (defaultSize**2); i++) {
   const box = document.createElement("div");
-  box.style.cssText = `width: calc(50vh/${size}); height: calc(50vh/${size}); 
+  box.style.cssText = `width: calc(50vh/${defaultSize}); height: calc(50vh/${defaultSize}); 
                        border-color: black; border-style: solid; border-width: 1px;`;
   box.setAttribute("class", "box");
   board.appendChild(box);
-}
-
-let boxes = document.getElementsByClassName("box");
+};
 
 for (let i = 0; i < boxes.length; i++) {
   boxes[i].addEventListener("mousemove",  () => {
@@ -18,12 +19,51 @@ for (let i = 0; i < boxes.length; i++) {
   });
 }
 
-
-
 for (let i = 0; i < boxes.length; i++) {
   boxes[i].addEventListener("touchmove",  () => {
     boxes[i].style.backgroundColor = "black";
   });
 }
 
+sizeInput.addEventListener("input", (event) => {
+  size = event.target.value;
+});
 
+sizeConfirm.addEventListener("click", () => {
+  if (defaultSize != size) {
+    // removing previously made boxes
+    
+    // need ref to size because as elements are removed boxes.length becomes smaller
+    for (let i = 0; i < (defaultSize**2); i++) {
+      boxes[0].remove();
+    }
+
+    // creating new boxes with new size parameter
+    for (let i = 0; i < (size**2); i++) {
+      const box = document.createElement("div");
+      box.style.cssText = `width: calc(50vh/${size}); height: calc(50vh/${size}); 
+                           border-color: black; border-style: solid; border-width: 1px;`;
+      box.setAttribute("class", "box");
+      board.appendChild(box);
+    };
+
+    // getting reference to new boxes
+    boxes = document.getElementsByClassName("box");
+    
+    // giving new boxes event listeners
+    for (let i = 0; i < boxes.length; i++) {
+      boxes[i].addEventListener("mousemove",  () => {
+        boxes[i].style.backgroundColor = "black";
+      });
+    }
+    
+    for (let i = 0; i < boxes.length; i++) {
+      boxes[i].addEventListener("touchmove",  () => {
+        boxes[i].style.backgroundColor = "black";
+      });
+    }
+
+    // set new default size in case user changes size again
+    defaultSize = size;
+  }
+});
